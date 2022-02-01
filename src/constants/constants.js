7d21f55b0f2784809497acf12ca0a4991297a62e
Rohit <rohitc437>
2022-02-01 18:47:28 +0530

    changes 4

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 export const projects = [
   {
     title: "Dunzo(clone)",
-    description: "I have in that Home page, Product page, Address details page",
+    description:
+      "A web application to serve online delivery at minimal cost and time. Feature :- Home page, Product page, Address details page",
     image: "/images/1.jfif",
     tags: ["CSS", "HTML", "JavaScript"],
     source: "https://github.com/mayur8600/Dunzo",
@@ -20,8 +21,6 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  // { year: 2011, text: "SSC from K.S.K.New city high school,Dhule" },
-  // { year: 2014, text: "HSC from V.W.S. College,Dhule" },
   { year: 2017, text: "Bachelor of Computer Science,(B.sc)" },
   { year: 2020, text: "Master of computer science,(M.sc)" },
   { year: 2022, text: "MERN Stack From Masai Institute" },
